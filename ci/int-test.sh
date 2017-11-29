7c6da4143287518f5364b66e7e727a35463f83a2
pivotal-choonghyun-oh <coh@pivotal.io>
2017-11-29 15:27:19 +0900

    aa

--- ci/int-test.sh
+++ ci/int-test.sh
@@ -3,7 +3,7 @@
 BASE_URL=$1
 
 echo "+====================================================+"
-echo "|   Spring Music Integeration Test : V2              |"
+echo "|   Spring Music Integeration Test : V3              |"
 echo "|   host : $BASE_URL                                  "
 echo "+====================================================+"
 
