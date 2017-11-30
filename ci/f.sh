c916315d474be89c6a5d79fb769d3d0b0118ed67
pivotal-choonghyun-oh <coh@pivotal.io>
2017-11-30 09:30:05 +0900

    aa

--- ci/f.sh
+++ ci/f.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-fly -t lite login -c http://192.168.219.102:8088 -u concourse -p changeme
+fly -t lite login -c http://60.93.213.81:8088 -u concourse -p changeme
 
 fly -t lite set-pipeline -p shdemo -c ../../p.yml -l ../../cred.yml
 
